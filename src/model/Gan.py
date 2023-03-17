caf5cc94aff5c26d5f59c6fba4dbb5f491fc6d8f
3uchen <92212586+3uchen@users.noreply.github.com>
2023-03-17 10:30:31 +0800

    Add files via upload

---
+++ src/model/Gan.py
@@ -0,0 +1,44 @@
+import torch.nn as nn
+
+class Generator(nn.Module):
+    def __init__(self, latent_dim, data_dim):
+        super(Generator, self).__init__()
+        self.data_dim = data_dim
+        self.latent_dim = latent_dim
+
+        def block(in_feat, out_feat, normalize=True):
+            layers = [nn.Linear(in_feat, out_feat)]
+            if normalize:
+                layers.append(nn.BatchNorm1d(out_feat, 0.8))
+            layers.append(nn.LeakyReLU(0.2, inplace=True))
+            return layers
+
+        self.model = nn.Sequential(
+            *block(self.latent_dim, 32, normalize=False),
+            *block(32, 32),
+            nn.Linear(32, self.data_dim),
+            nn.Sigmoid()
+        )
+
+    def forward(self, z):
+        img = self.model(z)
+        return img
+
+
+class Discriminator(nn.Module):
+    def __init__(self, data_dim):
+        super(Discriminator, self).__init__()
+        self.data_dim = data_dim
+
+        self.model = nn.Sequential(
+            nn.Linear(self.data_dim, 32),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(32, 32),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(32, 1),
+            nn.Sigmoid(),
+        )
+
+    def forward(self, img):
+        validity = self.model(img)
+        return validity
